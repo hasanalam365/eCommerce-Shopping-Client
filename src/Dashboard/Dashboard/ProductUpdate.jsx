4bbe23694a78c31279f63a1b9d056c895c93d6fd
Hasan Alam <hasanalam365@gmail.com>
2025-02-06 12:26:53 +0600

    product updated img and description

--- src/Dashboard/Dashboard/ProductUpdate.jsx
+++ src/Dashboard/Dashboard/ProductUpdate.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useAxiosSecure from '../../hooks/useAxiosSecure';
 import useAxiosPublic from '../../hooks/useAxiosPublic';
 import { toast } from 'react-toastify';
@@ -14,8 +14,15 @@ const ProductUpdate = () => {
      const axiosSecure = useAxiosSecure()
     const axiosPublic = useAxiosPublic()
 
-    const productData=useLoaderData()
+    const productData = useLoaderData()
+     const [imgPrev, setImgPrev] = useState('')
 
+      const handleImg = (e) => {
+
+        const photo = e.target.files[0];
+        setImgPrev(photo.name)
+
+    }
 console.log(productData)
 
      const handleUpdateProduct = async (e) => {
@@ -126,34 +133,46 @@ console.log(productData)
                                    placeholder="price" className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent " />
                            </div>
        
-                           <div className=' col-span-3 sm:col-span-3 text-center'>
-                               <label>
-                                   <input className='text-sm cursor-pointer w-36 hidden'
-                                       type='file'
-                                       name='photo'
-                                       id='image'
-                                       accept='image/*'
-       
-                                       required
-                                   />
-       
-       
-                                   <div className='bg-rose-500 text-white border border-gray-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-rose-500'>
-                                       Upload Image
-                                   </div>
-                               </label>
-                           </div>
-                           {
-                         
-                               <div className="col-span-3 sm:col-span-3 flex items-center text-red-600">
+                     <div className=' col-span-3 sm:col-span-3 text-center'>
+                        <label>
+                            <input onChange={handleImg} className='text-sm cursor-pointer w-36 hidden'
+                                type='file'
+                                name='photo'
+                                id='image'
+                                accept='image/*'
+
+                                required
+                            />
+
+
+                            <div className='bg-rose-500 text-white border border-gray-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-rose-500'>
+                                Upload Image
+                            </div>
+                        </label>
+                    </div>
+                    {imgPrev ? <div className="col-span-3 sm:col-span-3 flex items-center ">
+                        {imgPrev}
+                    </div> :
+                        <div className='col-span-3 sm:col-span-3 flex  items-center gap-10'>
+                               <div className=" text-red-600   w-1/2">
                                    No file Select
-                               </div>
-                           }
+                        </div>
+                        <div>
+                        <img src={productData?.imgUrl}
+                        className='w-8 h-8'
+                            alt="" />
+                    </div>
+                            </div>
+                    }
+                    
+ 
+
                            <div className="col-span-6 sm:col-span-6">
                                <label htmlFor="description" className="font-medium">Description</label>
        
                                <textarea
                                    name="description"
+                                   defaultValue={productData?.description}
                                    className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                                    rows="4"
                                    placeholder="Enter your description here..."
